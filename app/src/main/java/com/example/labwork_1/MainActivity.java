c81bdab2c9e03f01be73f73edacdf550e5c971c0
annneskor <annnesk@gmail.com>
2024-11-04 17:15:34 +0300

    labwork1

---
+++ app/src/main/java/com/example/labwork_1/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.labwork_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Переход к Activity 2
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                // Передача параметра
+                intent.putExtra("parameter", "Нескоромная");
+
+                startActivity(intent);
+            }
+
+
+        })
+    ;}
+
+
+}
---
+++ app/src/main/java/com/example/labwork_1/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.labwork_1;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+
+public class SecondActivity extends AppCompatActivity {
+    TextView textView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textView1 = findViewById(R.id.textView1);
+
+        // Получение параметра из Activity 1
+        String parameter = getIntent().getStringExtra("parameter");
+
+        // Вывод текста в TextView
+        textView1.setText("Переданный параметр: " + parameter);
+
+    }
+}
